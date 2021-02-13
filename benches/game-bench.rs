68e7a2496784161dd7b202dc8e778432aca6590b
Thomas Koster <tkoster@gmail.com>
2021-02-13 15:00:40 +1100

    Perform allocation/init outside of benchmark
    The benchmarks were including buffer allocation and game state
    initialization in the time measurements. These have been moved to a
    setup phase using iter_batched_ref.
    
    New results:
    
    game::step_generation   2.02 ms (+0%)
    game::draw_generation   2.00 ms (-34%)
    game::update            4.04 ms (-18%)

--- benches/game-bench.rs
+++ benches/game-bench.rs
@@ -1,4 +1,4 @@
-use criterion::{ black_box, criterion_group, criterion_main, Criterion };
+use criterion::{ Criterion, BatchSize, black_box, criterion_group, criterion_main };
 use gameoflife as game;
 
 const WIDTH: usize = 1280;
@@ -7,35 +7,52 @@ const CELL_SIZE: u32 = 2;
 const BYTES_PER_PIXEL: usize = 3;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("game::step_generation", |b| b.iter(|| {
-        let mut state = game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE);
-        game::step_generation(black_box(&mut state), 1)
-    }));
+    c.bench_function("game::step_generation", |bencher| bencher.iter_batched_ref(
+        || {
+            game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE)
+        },
+        |state| {
+            game::step_generation(black_box(state), 1)
+        },
+        BatchSize::PerIteration
+    ));
 
-    c.bench_function("game::draw_generation", |b| b.iter(|| {
-        let mut pixels = vec![0; WIDTH * HEIGHT * BYTES_PER_PIXEL];
-        let mut buffer = game::ImageBuffer {
-            width:  WIDTH,
-            height: HEIGHT,
-            pitch:  WIDTH * BYTES_PER_PIXEL,
-            pixels: pixels.as_mut_slice()
-        };
-        let state = game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE);
-        game::draw_generation(&mut buffer, 1, &state);
-    }));
+    c.bench_function("game::draw_generation", |bencher| bencher.iter_batched_ref(
+        || {
+            let pixels = vec![0; WIDTH * HEIGHT * BYTES_PER_PIXEL];
+            let state = game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE);
+            (pixels, state)
+        },
+        |(pixels, state)| {
+            let mut buffer = game::ImageBuffer {
+                width:  WIDTH,
+                height: HEIGHT,
+                pitch:  WIDTH * BYTES_PER_PIXEL,
+                pixels: pixels.as_mut_slice()
+            };
+            game::draw_generation(&mut buffer, 1, state)
+        },
+        BatchSize::PerIteration
+    ));
 
-    c.bench_function("game::update", |b| b.iter(|| {
-        let mut pixels = vec![0; WIDTH * HEIGHT * BYTES_PER_PIXEL];
-        let mut buffer = game::ImageBuffer {
-            width:  WIDTH,
-            height: HEIGHT,
-            pitch:  WIDTH * BYTES_PER_PIXEL,
-            pixels: pixels.as_mut_slice()
-        };
-        let mut state = game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE);
-        let events = NoEventsIterator {};
-        game::update(1, &mut buffer, &mut state, events);
-    }));
+    c.bench_function("game::update", |bencher| bencher.iter_batched_ref(
+        || {
+            let pixels = vec![0; WIDTH * HEIGHT * BYTES_PER_PIXEL];
+            let state = game::init((WIDTH as u32, HEIGHT as u32), CELL_SIZE);
+            (pixels, state)
+        },
+        |(pixels, state)| {
+            let mut buffer = game::ImageBuffer {
+                width:  WIDTH,
+                height: HEIGHT,
+                pitch:  WIDTH * BYTES_PER_PIXEL,
+                pixels: pixels.as_mut_slice()
+            };
+            let events = NoEventsIterator {};
+            game::update(1, &mut buffer, state, events)
+        },
+        BatchSize::PerIteration
+    ));
 }
 
 struct NoEventsIterator {
